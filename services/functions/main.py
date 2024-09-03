77060cbd8fadfc7138dc54d2285af2242240c678
Nishandd <nishant.dahiya2000@gmail.com>
2024-09-04 03:33:40 +0530

    Implement getProfile and getLinks

---
+++ services/functions/main.py
@@ -0,0 +1,54 @@
+import requests
+import pandas as pd
+import os
+from bs4 import BeautifulSoup as bs
+
+def printErr(e: Exception) -> None:
+    print("ERROR :: ", e)
+
+def getLinks(name: str, research_papers: str) -> list:
+    """
+    Takes name of the faculty member and title of their research papers and returns the links for publications of those research papers
+    """
+    try:
+        links = []
+        for paper in research_papers:
+            query = f"{name} {paper}"
+            url = f"https://www.google.com/search?q={query}"
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+            response = requests.get(url, headers=headers)
+            soup = bs(response.text, 'html.parser')
+            search_results = soup.find_all('div', class_='yuRUbf')
+            if search_results:
+                first_link = search_results[0].find('a')['href']
+                links.append(first_link)
+            
+        return links
+    except Exception as e:
+        printErr(e)
+        return []
+    
+def getProfile(path: str) -> list:
+    """
+    Takes path for excel sheet and returns faculty's profile of research papers
+    """
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), path))
+    try:
+        df = pd.read_excel(path)
+    except Exception as e:
+        printErr(e)
+        return []
+    
+    try:
+        faculty_list = df.iloc[1:, 2].tolist()
+        name = df.iloc[1, 0]
+        try:
+            links: list = getLinks(name, faculty_list)
+            return links
+        except:
+            raise Exception("API ERROR :: Couldn't fetch data from google")
+    except Exception as e:
+        printErr(e)
+        return []
+
+getProfile("../../static/assets/demo.xlsx")
