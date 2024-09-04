759064cd8d692731d870df0f67bc4e14d472dfab
Prathya Thakur <98422626+thakurprathya@users.noreply.github.com>
2024-09-05 04:52:33 +0530

    updated home page, wrote basic backend...

--- app.py
+++ app.py
@@ -4,27 +4,23 @@ from flask import Flask, render_template, request, redirect, url_for, render_tem
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 @app.route('/')
 def home():
     return render_template('pages/index.html')
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
         file = request.files.get('file')
         if file:
             df = pd.read_excel(file)
-            data = df.to_json(orient='split')
-            return data, 200, {'Content-Type': 'application/json'}
-        return {'error': 'Invalid file type or missing file'}, 400
+            # processing of file input data
+            return {'message': 'File Saved Successfuly'}, 200
+    return {'error': 'Invalid file type or missing file'}, 400
 
-    return render_template('pages/upload.html')
+@app.route('/download')
+def download():
+    return render_template('pages/download.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
